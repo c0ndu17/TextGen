5c3c53fbab7c404f5f9757be098bbaac7970cddc
GWA PHILLIPS <16512561@h74.(none)>
2012-10-21 20:16:12 +0200

    v1.0

---
+++ src/java/MarkovModel.java
@@ -0,0 +1,21 @@
+public class MarkovModel{
+	private int Order;
+	MarkovModel(String text, int k){
+
+	}
+	
+	int order(){
+		try{
+			return order
+		}
+		catch(Exception exc){
+			System.out.println("The order has not been set yet, a NullPointer:xception has been thrown!!!");
+		}
+
+	int freq(){
+
+
+
+
+
+}
